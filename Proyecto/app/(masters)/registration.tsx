9563acdc63a1bd98c4b27b230c79e1dc96ac1fd7
Jonnathan Alegria <jonathanalegria01@gmail.com>
2025-01-31 23:04:00 -0600

    Agregadas las validaciones de formulario Agregada la logica del manejo de registro

--- Proyecto/app/(masters)/registration.tsx
+++ Proyecto/app/(masters)/registration.tsx
@@ -1,52 +1,69 @@
 import { useState } from "react";
-import { ScrollView, Text, StyleSheet, TextInput, Button } from "react-native";
+import { ScrollView, Text, StyleSheet, TextInput, Button, Alert } from "react-native";
 
 export default function RegistrationScreen() {
     const [formulario, setFormulario] = useState({
-        nombre: 'prueba',
-        apellido: 'test',
+        nombre: '',
+        apellido: '',
         correo: '',
-        telefono: '1234',
+        telefono: '',
         password: '',
         confirmarPassword: '',
     });
 
     const [errores, setErrores] = useState({
-        nombre: 'prueba',
-        apellido: 'test',
+        nombre: '',
+        apellido: '',
         correo: '',
-        telefono: '1234',
+        telefono: '',
         password: '',
         confirmarPassword: '',
     });
 
+    const validarFormulario = () => {
+       let erroresTemp: any = {};
+       
+       if (!formulario.nombre.trim()) erroresTemp.nombre = 'El nombre es obligatorio';
+       if (!formulario.apellido.trim()) erroresTemp.apellido = 'El apellido es obligatorio';
+       if (!formulario.correo.includes('@')) erroresTemp.correo = 'Correo Invalido';
+       if (formulario.telefono.length < 8) erroresTemp.telefono = 'Numero Invalido';
+       if (formulario.password.length < 4) erroresTemp.password = 'Minimo 4 caracteres';
+       if (formulario.password !== formulario.confirmarPassword) erroresTemp.confirmarPassword = 'Contraseñas no coinciden';
+
+       setErrores(erroresTemp);
+    };
+
     const manejarCambio = (campo: string, valor: string) => {
+        validarFormulario();
         setFormulario((prevFormulario) => ({
             ...prevFormulario,
             [campo]: valor,
         }));
     }
-    const validarFormulario = () => {
-        
-    };
-
     const manejarRegistro = () => {
-    
+        validarFormulario();
+        if (Object.keys(errores).length === 0) {
+            //Alert.alert('Registro exitoso', `Bienvenido ${formulario.nombre} ${formulario.apellido}`);
+            Alert.alert('Registro exitoso', "Bienvenido " + formulario.nombre + " " + formulario.apellido);
+        }else{
+            Alert.alert('Error', 'Por favor corrija los campos');
+        }
     };
 
     return (
         <ScrollView contentContainerStyle={styles.Container}>
             <Text>Registro</Text>
             <Text style={styles.label}>
-                Nombre:
+                Nombre: {formulario.nombre}
             </Text>
-
             <TextInput
                 style={styles.input}
                 placeholder="Ingrese su nombre"
                 value={formulario.nombre}
                 onChangeText={(texto) => { manejarCambio('nombre', texto) }}
             />
+            {errores.nombre && <Text style={styles.error}>{errores.nombre}</Text>}
+
             <Text style={styles.label}>
                 Apellido:
             </Text>
@@ -56,6 +73,8 @@ export default function RegistrationScreen() {
                 value={formulario.apellido}
                 onChangeText={(texto) => { manejarCambio('apellido', texto) }}
             />
+            {errores.apellido && <Text style={styles.error}>{errores.apellido}</Text>}
+
             <Text style={styles.label}>Correo Electronico: </Text>
             <TextInput
                 style={styles.input}
@@ -65,6 +84,7 @@ export default function RegistrationScreen() {
                 value="formulario.correo"
                 onChangeText={(texto) => { manejarCambio('correo', texto) }}
             />
+            {errores.correo && <Text style={styles.error}>{errores.correo}</Text>}
 
             <Text>Numero telefonico:</Text>
             <TextInput
@@ -75,7 +95,8 @@ export default function RegistrationScreen() {
                 value={formulario.telefono}
                 onChangeText={(texto) => { manejarCambio('telefono', texto) }}
             />
-            
+            {errores.telefono && <Text style={styles.error}>{errores.telefono}</Text>}
+        
             <Text>Contraseña</Text>
             <TextInput 
             style={styles.input}
@@ -84,6 +105,7 @@ export default function RegistrationScreen() {
             value={formulario.password}
             onChangeText={(texto) => { manejarCambio('password', texto) }}
             />
+            {errores.password && <Text style={styles.error}>{errores.password}</Text>}
 
             <Text>Confirmar Contraseña</Text>
             <TextInput 
@@ -93,15 +115,22 @@ export default function RegistrationScreen() {
             value={formulario.confirmarPassword}
             onChangeText={(texto) => { manejarCambio('confirmarPassword', texto) }}
             />
+            {errores.confirmarPassword && <Text style={styles.error}>{errores.confirmarPassword}</Text>}
 
             <Button title="Registrarse" onPress={manejarRegistro}/>
 
         </ScrollView>
     );
+    
 };
 
 
 const styles = StyleSheet.create({
+    error:{
+        color: 'red',
+        fontSize: 12,
+        margin:10,
+    },
     Container: {
         flexGrow: 1,
         justifyContent: 'center',
