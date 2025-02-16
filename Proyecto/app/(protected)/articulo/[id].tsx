0bd58f18eda13fa7a50bf13f7e75f222c604c87a
Jonnathan Alegria <jonathanalegria01@gmail.com>
2025-02-16 10:08:32 -0600

    Añadiendo estilización a todos los componentes creados anteriormente

--- Proyecto/app/(protected)/articulo/[id].tsx
+++ Proyecto/app/(protected)/articulo/[id].tsx
@@ -1,6 +1,7 @@
 import ArticuloCard from "@/components/ArticuloCard";
+import { Ionicons } from "@expo/vector-icons";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import { Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function ArticuloScreen(){
 
@@ -8,7 +9,45 @@ export default function ArticuloScreen(){
     const {id, nombre, categoria, cantidad} = useLocalSearchParams();
 
     return(
+        <View style={Styles.container}>
         <ArticuloCard nombre={nombre as string} categoria={categoria as string} stock={Number(cantidad)}/>
-        
-    )
+
+        <TouchableOpacity style={Styles.backButton} onPress={()=> router.back()}>
+            <Ionicons name="arrow-back-outline" size={24} color="white" />
+            <Text style={Styles.backButtonText}>Volver al Inventario</Text>
+        </TouchableOpacity>
+        </View>
+    );
 }
+
+const Styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+        backgroundColor:"#F5F7FA"
+    },
+    backButton: {
+        flexDirection: "row",
+        backgroundColor: "#4A90E2",
+        padding:15,
+        borderRadius:10,
+        alignItems:"center",
+        justifyContent:"center",
+        width:"80%",
+        marginTop:20,
+        shadowColor:"#000",
+        shadowOpacity:0.2,
+        shadowRadius:5,
+        elevation:5,
+
+    },
+    backButtonText: {
+        color:"white",
+        fontSize:16,
+        fontWeight:"bold",
+        marginLeft:10,
+    },
+
+});
